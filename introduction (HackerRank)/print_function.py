a0a8a3f99f69049213d3334669e7c1cbb403704a
aastha <aasthaaggarwal018@gmail.com>
2026-03-20 18:22:41 +0530

    Create print_function.py

---
+++ introduction (HackerRank)/print_function.py
@@ -0,0 +1,7 @@
+#The included code stub will read an integer,n, from STDIN.
+#Without using any string methods, try to print the following:
+#123.......
+
+n = int(input())
+for i in range(0,n):
+    print(i+1,end='')
