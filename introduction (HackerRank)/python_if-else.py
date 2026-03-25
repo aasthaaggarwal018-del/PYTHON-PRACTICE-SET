7c0ec4472f3c9302aa9ec7f518439f2af3f38ea7
aastha <aasthaaggarwal018@gmail.com>
2026-03-25 13:42:50 +0530

    Update python_if-else.py

--- introduction (HackerRank)/python_if-else.py
+++ introduction (HackerRank)/python_if-else.py
@@ -7,10 +7,10 @@ If a is even and in the inclusive range of 6 to 20, print Weird
 If a is even and greater than 20, print Not Weird 
 '''
 
-a = int(input())
+a = int(input('entre your number:'))
 if a % 2 != 0:
     print('Weird')
-elif 6<a<20:
+elif 6<=a<=20:
     print('Weird')
 else:
     print('Not Weird')
