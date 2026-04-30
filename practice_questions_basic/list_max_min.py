b30b452c245bef107684cf23972ed9d2f9bb76e2
aastha <aasthaaggarwal018@gmail.com>
2026-04-30 21:25:42 +0530

    Create list_max_min.py

---
+++ practice_questions_basic/list_max_min.py
@@ -0,0 +1,10 @@
+'''Practice Problem: Given a list of integers,
+ find and print both the largest and the smallest numbers.'''
+
+list=[56,34,34,89,9,54,24]
+
+print(f'max number : {max(list)} \nmin number : {min(list)}')
+
+
+
+    
