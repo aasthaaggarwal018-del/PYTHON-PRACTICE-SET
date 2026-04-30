009883ee00b59407184bb3eb6b86cea311e4a27a
aastha <aasthaaggarwal018@gmail.com>
2026-04-30 21:25:45 +0530

    Create word_reverse.py

---
+++ practice_questions_basic/word_reverse.py
@@ -0,0 +1,19 @@
+'''Practice Problem:
+ Write a program that takes a string and reverses it (e.g., “Python” becomes “nohtyP”).'''
+
+
+
+def reverse(x):
+    word=x[::-1]
+    return word
+        
+   
+
+
+print(reverse('python'))
+print(reverse('aastha'))
+
+    
+
+
+
