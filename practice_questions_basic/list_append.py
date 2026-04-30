cff4fa21c4ea8c1b1906ad6454b3deefcc4040d0
aastha <aasthaaggarwal018@gmail.com>
2026-04-30 21:25:35 +0530

    Create list_append.py

---
+++ practice_questions_basic/list_append.py
@@ -0,0 +1,13 @@
+'''Practice Problem: Create a list of 5 fruits.
+ Add a new fruit to the end of the list, then remove the second fruit (at index 1).'''
+
+
+
+fruit=['apple','kiwi','mango','pineapple','grape']
+
+fruit.append('orange')
+fruit.pop(1)
+
+
+print(fruit)
+
